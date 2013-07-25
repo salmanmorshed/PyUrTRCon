68106092f3eba4ebe953267a4557bbcedb9390f8
Md. Salman Morshed <salmanmorshed@gmail.com>
2013-07-25 21:42:58 +0600

    Added a few more test tab-complete cases

--- tabsupport.py
+++ tabsupport.py
@@ -1,10 +1,14 @@
 #! /dev/null
 
+# Contains all the commands that RCon supports to provide tab-complete support.
+
 export_data = [
 	"g_nextmap",
 	"g_gametype",
 	"g_gear",
-	"map"
+	"bigtext",
+	"map",
+	"exec",
 ]
 
 if __name__ == "__main__" : pass
