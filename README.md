7e2b7c04feeda9481be71fc44e063f5eef0463d4
Md. Salman Morshed <salmanmorshed@gmail.com>
2016-01-19 00:13:12 +0600

    refactor codebase into single .py file & misc changes

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
 PyUrTRCon
 =========
 
-A small tool for Urban Terror (or any other similar Quake3 based games) server administrators written in Python that can execute RCon commands on remote servers.
+A small tool for Urban Terror (or any other similar Quake3-based games) server administrators written in Python that can execute RCon commands on remote servers.
+
+
+Usage
+-----
+The tool is a Python 2.7.x script. Start it using the python interpreter.
+	
+	$ python pyurtrcon.py
+	
+Optionally, we can pass it the game-server address, port and the password.
+
+	$ python pyurtrcon.py urtbd.com:27960 12345
+	
+The game-server information can also be set during a session using the `gameserver` and `password` commands (see below).
+
+Once in a session, type `help` to get a list of available commands and their usages.
+
+#### Common Commands
+
++ The `rcon` command sends RCon request to the server. Examples:
+
+	> \>\> rcon g\_nextmap ut4\_turnpike
+	
+	> \>\> rcon cyclemap
+
++ The `history` command lists all previously executed commands during the current session.
++ The `gameserver` command sets the address and port number of the game-server.
++ The `password` command sets the RCon password for the game-server. 
+
+#### Auto-complete
+Press the `tab` button to auto-complete partially typed commands. It can also complete some RCon commands as well. You can extend the list of RCon commands the tool can auto-complete by editing the script. Add the commands you frequently use in this list:
+
+	common_commands = ['exec', 'bigtext', 'map', 'cyclemap', 'g_nextmap', 'g_gametype']
+
+Also, add servers you frequently manage in this list:
+	
+	common_addresses = []
+
+License
+-------
+This software is released under the [GNU General Public License, version 2](http://opensource.org/licenses/gpl-2.0.php).
--- pyurtrcon.py
+++ pyurtrcon.py
@@ -1,119 +1,118 @@
-#! /usr/bin/python
+#!/usr/bin/env python
 
-import sys, cmd, socket, readline, re
+import sys
+import cmd
+import socket
+import re
+
 
 class Console(cmd.Cmd):
+    magic = '\377\377\377\377'
+    timeout = 5
+    common_commands = ['exec', 'bigtext', 'map', 'cyclemap', 'g_nextmap', 'g_gametype']
+    common_addresses = []
+    server_host = '127.0.0.1'
+    server_port = 27960
+    rcon_password = ''
 
-	magic = "\377\377\377\377"
-	server_host = "localhost"
-	server_port = 27960
-	rcon_password = ""
-	commands = []
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = '>> '
+        self.intro  = 'Welcome to PyUrTRCon. Type "help" or "?" for instructions.'
+        try:
+            import readline
+        except:
+            self.intro += '\n(!) "readline" not available. Autocomplete will not work.'
+
+    def do_gameserver(self, args):
+        """Set or display the gameserver address.\nSupply the address in host:port format to set it."""
+        if args == '':
+            print('Address of the gameserver is "{}:{}"'.format(self.server_host, self.server_port))
+        else:
+            self.server_host = args.split(':')[0]
+            self.server_port = int(args.split(':')[1])
+
+    def do_password(self, args):
+        """Set or display the RCon password."""
+        if args == '':
+            print('RCon password is "{}"'.format(self.rcon_password))
+        else:
+            self.rcon_password = args
+
+    def do_rcon(self, args):
+        """Send RCon commands to UrT server."""
+        try:
+            socket_handle = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            socket_handle.connect((self.server_host, self.server_port))
+            socket_handle.settimeout(self.timeout)
+            socket_handle.send('{}rcon {} {}'.format(self.magic, self.rcon_password, args))
+            (response, address) = socket_handle.recvfrom(1024)
+            socket_handle.close()
+            formatted_response = re.sub(r'\^.', '', response.split('\n')[1])
+            if len(formatted_response):
+                print(formatted_response)
+        except Exception as e:
+            print('Error! Details: {}'.format(e.message))
+
+    def complete_rcon(self, primitive, *_):
+        if primitive:
+            return [ command for command in self.common_commands if command.startswith(primitive) ]
+        else:
+            return self.common_addresses
+
+    def do_history(self, _):
+        """Prints all the previous commands of this session."""
+        for entry in self._hist:
+            print(entry)
+
+    def do_help(self, args):
+        """'help' or '?' with no arguments will print all commands.\n'help <command>' or '? <command>' will print help on <command>."""
+        cmd.Cmd.do_help(self, args)
+
+    def do_exit(self, _):
+        """Exits from the PyUrTRCon tool."""
+        return -1
+
+    def do_EOF(self, _):
+        return -1
+
+    def preloop(self):
+        self._hist  = []
+        self._locals  = {}
+        self._globals = {}
+        cmd.Cmd.preloop(self)
+
+    def postloop(self):
+        cmd.Cmd.postloop(self)
+
+    def precmd(self, line):
+        self._hist += [ line.strip() ]
+        return line
+
+    def postcmd(self, stop, line):
+        return stop
+
+    def emptyline(self):   
+        pass
+
+    def default(self, line):      
+        print('Unrecognized command. Type "help" for instructions.')
+
+    undoc_header = None
+
+    def print_topics(self, header, cmds, cmdlen, maxcol):
+        if header is not None:
+            cmd.Cmd.print_topics(self, header, cmds, cmdlen, maxcol)
 
 
-	def __init__(self) :
-		cmd.Cmd.__init__(self)
-		self.prompt = "\n-> "
-		self.intro  = "\nWelcome to PyUrTRCon. Type help for support."
-
-
-	def do_gameserver(self, args) :
-		"""Set or display the gameserver address. Supply the address in host:port format to set it."""
-		if args == "" :
-			print "Address of the gameserver is: \"%s:%d\"" % (self.server_host, self.server_port)
-		else:
-			self.server_host = args.split(":")[0]
-			self.server_port = int(args.split(":")[1])
-
-
- 	def do_password(self, args) :
- 		"""Set or display the RCon password for the current session."""
-		if args == "" :
-			print "RCon password is: \"" + self.rcon_password + "\""
-		else:
-			self.rcon_password = args
-
-
-	def do_rcon(self, args) :
-		"""Send RCon commands to UrT server."""
-		try:
-			socket_handle = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-			socket_handle.connect((self.server_host, self.server_port))
-			socket_handle.settimeout(5)
-			socket_handle.send(self.magic + "rcon " + self.rcon_password + " " + args)
-			(response, address) = socket_handle.recvfrom(1024)
-			socket_handle.close()
-			formatted_response = re.sub(r'\^.', '', response.split("\n")[1])
-			if len(formatted_response) :
-				print formatted_response
-		except Exception, e:
-			print "Error encountered! Details: " + str(e)
-
-
-	def complete_rcon(self, primitive, line, start_index, end_index):
-		if primitive:
-			return [ command for command in self.commands if command.startswith(primitive) ]
-		else:
-			return self.addresses
-
-
-	def do_history(self, args) :
-		"""Prints all the previous commands of this session."""
-		print self._hist
-
-
-	def do_help(self, args) :
-		"""'help' or '?' with no arguments will print all commands.\n'help <command>' or '? <command>' will print help on <command>."""
-		cmd.Cmd.do_help(self, args)
-
-
-	def do_exit(self, args) :
-		"""Exits from the PyUrTRCon tool."""
-		return -1
-
-
-	def preloop(self) :
-		cmd.Cmd.preloop(self)
-		self._hist	= []
-		self._locals  = {}
-		self._globals = {}
-
-
-	def postloop(self) :
-		cmd.Cmd.postloop(self)
-
-
-	def precmd(self, line) :
-		self._hist += [ line.strip() ]
-		return line
-
-
-	def postcmd(self, stop, line) :
-		return stop
-
-
-	def emptyline(self) :	
-		"""Do nothing on empty input line"""
-		pass
-
-
-	def default(self, line) :	   
-		print "Unrecognized command. Type \"help\" for instructions."
-
-
-def main() :
-	console = Console()
-	if len(sys.argv) >= 2 :
-		console.server_host = sys.argv[1].split(":")[0] if (':' in sys.argv[1]) else sys.argv[1]
-		console.server_port = int(sys.argv[1].split(":")[1]) if (':' in sys.argv[1]) else 27960
-	if len(sys.argv) >= 3 :
-		console.rcon_password = sys.argv[2]
-	try :
-		import tabsupport
-		console.commands = tabsupport.export_data
-	except Exception, e:
-		console.intro = console.intro + "\n[!] Tab-completion not available."
-	console.cmdloop()
+def main():
+    console = Console()
+    if len(sys.argv) >= 2:
+        console.server_host = sys.argv[1].split(':')[0] if (':' in sys.argv[1]) else sys.argv[1]
+        console.server_port = int(sys.argv[1].split(':')[1]) if (':' in sys.argv[1]) else 27960
+    if len(sys.argv) >= 3:
+        console.rcon_password = sys.argv[2]
+    console.cmdloop()
 
 
 if __name__ == '__main__': main()
